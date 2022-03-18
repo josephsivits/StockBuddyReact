62ee07bde7995cf19c99b5712926e0df7ce3b4ff
Joseph Sivits <josephsivits@gmail.com>
2022-03-18 17:28:40 -0500

    added config file

---
+++ app/config/colors.js
@@ -0,0 +1,18 @@
+export default {
+    // colors in stock grading
+    lightNlue: '#32d7ff',
+    offBlueGreen: '#13ff8b',
+    greenYellow: '#80ff4a',
+    gold: '#fcde42',
+    mustardYellow: '#fcff4a',
+    redOrange: '#fc5b0c',
+    red: '#ff0909',
+
+    // color palette
+    primary: '#ccbbff',
+    secondary: '#ffd1fc',
+    dark: '#141414',
+    light: '#f5f5f5',
+    blueComplementary: '#497cd5',
+
+}
