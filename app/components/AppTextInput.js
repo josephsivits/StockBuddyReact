942635efdbfee4e087c8be85ea2043c90e1e4410
Joseph Sivits <josephsivits@gmail.com>
2022-03-18 18:17:27 -0500

    before colors extension in vscode

---
+++ app/components/AppTextInput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, TextInput, StyleSheet} from 'react-native';
+import {MaterialCommunityIcons} from '@expo/vector-icons';
+
+import colors from '../config/colors';
+import defaultStyles from '../config/defaultStyles';
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor:colors.light,
+    }
+})
